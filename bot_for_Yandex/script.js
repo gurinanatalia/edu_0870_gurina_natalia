21da4161a2be05d03d809a86d37d17d944ef662d
gurinanatalia <71058937+gurinanatalia@users.noreply.github.com>
2020-09-14 15:10:02 +0300

    14.09
    1) добавлена возможность ходить по сайту crushdrummers.ru, для чего сделаны: 
    - сайт добавлен в @match,
    - добавлен объект sites с 2-мя ключами (названиями сайтов) и свойствами (массивами и ключевых слов),
    - добавлены cookie: document.cookie и getCookie, document.cookie записывается 1 раз, когда мы находимся на главной странице поисковика, 
    2) обновлен способ обращения к кнопкам "найти" и "дальше" в яндексе, т.к. предыдущая версия перестала работать

--- bot_for_Yandex/script.js
+++ bot_for_Yandex/script.js
@@ -6,36 +6,53 @@
 // @author       You
 // @match        https://yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ['Гобой', 'Как звучит флейта', 'Кларнет', 'Корнет', 'Багет'];
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai": ["гобой", "как звучит флейта", "кларнет", "саксофон"],
+    "crushdrummers.ru": ["Барабанное шоу", "Заказать барабанное шоу", "Шоу барабанщиков в Москве"]
+};
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+let keywords = sites[site];
+
 let keyword = keywords[getRandom(0,keywords.length)];
-let search_button = document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0];
+let search_button = document.querySelector("button[class^='button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited']");
 let yandexInput = document.getElementById('text');
 let i=0;
 let links = document.links;
-let next_button = document.querySelector("a[aria-label$='Следующая страница']");
+let next_button = document.querySelector("a[aria-label^='Следующая страница']");
+
+if(search_button != undefined){
+    document.cookie = "site="+site;
+    //document.cookie = `site=${site}`;
+}else if (location.hostname == "yandex.ru"){
+    site = getCookie("site");
+}else{
+    site = location.hostname;
+}
 
 
 if(search_button != undefined){
+    document.cookie = "site="+site;
     let timerId = setInterval(()=>{
     yandexInput.value += keyword[i];
     i++;
     if(i==keyword.length){
         clearInterval(timerId);
         search_button.click();}
-}, getRandom(1000,1500));
+}, 500);
 
-}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
-    console.log("Мы на музыкалке");
+}else if(location.hostname == site){
+    
     setInterval(
         ()=>{
             let index = getRandom(0,links.length);
             if(getRandom(0,101)>=80){
                 location.href = "https://yandex.ru/";
             }
-            else if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+            else if(links[index].href.indexOf(site) != -1){
             links[index].click();
             }
         },
@@ -45,7 +62,7 @@ if(search_button != undefined){
 
     let yandexNextPage = true;
     for(let i=0;i<links.length;i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+        if(links[i].href.indexOf(site) != -1){
             console.log("Ссылка найдена "+links[i]);
             yandexNextPage = false;
             links[i].removeAttribute('target');
@@ -67,5 +84,12 @@ if(search_button != undefined){
 
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
