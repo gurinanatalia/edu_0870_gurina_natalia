6471d2b45bed435762e8669a22d93f12158941b6
gurinanatalia <71058937+gurinanatalia@users.noreply.github.com>
2020-09-14 15:13:01 +0300

    Классная работа 14.09
    Добавлена возможность боту ходить по 2м разным сайтам

--- bot_for_Goolge/script.js
+++ bot_for_Goolge/script.js
@@ -5,18 +5,36 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
+// @match        https://crushdrummers.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai": ["гобой", "как звучит флейта", "кларнет", "саксофон"],
+    "crushdrummers.ru": ["Барабанное шоу", "Заказать барабанное шоу", "Шоу барабанщиков в Москве"]
+};
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+let keywords = sites[site];
+
 let googleInput = document.getElementsByName('q')[0];
 let i = 0;
-let keywords = ["гобой", "как звучит флейта", "кларнет", "саксофон"];
+
 let keyword = keywords[getRandom(0,keywords.length)];
 let btnK = document.getElementsByName('btnK')[0];
 let links = document.links;
 
 if(btnK != undefined){
+    document.cookie = "site="+site;
+}else if(location.hostname == "www.google.com"){
+    site = getCookie("site");
+}else{
+    site = location.hostname;
+}
+
+
+if(btnK != undefined){
+    document.cookie = "site="+site;
     let timerId = setInterval(()=>{
     googleInput.value += keyword[i];
     i++;
@@ -26,15 +44,14 @@ if(btnK != undefined){
     }
  }, 700);
 
-}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
-    console.log("Мы на музыкалке");
+}else if(location.hostname == site){
     setInterval(
         ()=>{
             let index = getRandom(0,links.length);
             if(getRandom(0,101)>80){
                 location.href = 'https://www.google.com/';
             }
-            else if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+            else if(links[index].href.indexOf(site) != -1){
                links[index].click();
             }
         }, getRandom(1000,3000)
@@ -43,7 +60,7 @@ if(btnK != undefined){
 }else{
     let nextGooglePage = true;
     for(let i=0; i<links.length; i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+        if(links[i].href.indexOf(site) != -1){
             let link = links[i];
             nextGooglePage = false;
             setTimeout(()=>{link.click();},getRandom(1000,4000));
@@ -62,4 +79,11 @@ if(btnK != undefined){
 
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
